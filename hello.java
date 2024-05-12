8d6fbdc443c48d3ba12a61a7990281d1b28decf0
Manojkumarg9043 <manojvasanth57985@gmail.com>
2024-05-12 08:56:35 +0530

    change :  i change some of the code

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 public class hello{
     public static void main(String[] args) {
         System.out.println("This not My Code");
+
+        for(int i=0;i<10;i++){
+            System.out.println("Hi");
+        }
     }
 }
